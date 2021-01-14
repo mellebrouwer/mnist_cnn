a3c2867a0d8fb6fb12c8a21fc4a69da037faef97
Melle Brouwer <mellebrouwer1992@gmail.com>
2021-01-14 13:15:42 +0100

    Added untweaked CNN
    Untweaked CNN results in accuracy of ~97.5%

--- src/main.py
+++ src/main.py
@@ -100,3 +100,72 @@ mcm = multilabel_confusion_matrix(y_test, preds)
 
 print("\nNEURAL NET BASELINE")
 print(f"Accuracy: {acc}")
+
+# ---------------------------------------------
+# PREPARE & SPLIT DATASETS RESHAPE TO ndim=4
+# ---------------------------------------------
+
+# Reassign original arrays
+X = imagearray.reshape(-1, 28, 28, 1)
+y = labelarray
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
+
+# ---------------------------------
+# CONVOLUTIONAL NEURAL NETWORK
+# ---------------------------------
+
+# Input shape & random state
+tf.random.set_seed(0)
+
+# Build model
+model = keras.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+# Compile model
+model.compile(
+             optimizer="rmsprop",
+             loss="sparse_categorical_crossentropy",
+             metrics=["sparse_categorical_accuracy"],
+             )
+
+# Set early stopping
+early_stop = [keras.callbacks.EarlyStopping(
+                    monitor="val_loss",
+                    min_delta=1e-2,
+                    patience=3,
+                    verbose=1,
+                    )]
+    
+# Train model
+history = model.fit(
+                    X_train,
+                    y_train,
+                    batch_size=64,
+                    epochs=30,
+                    callbacks=early_stop,
+                    validation_split=0.2,
+                    )
+
+# Test model
+preds = model.predict(X_test)
+
+# Conver softmax probabilities to integers
+preds = np.argmax(preds,axis=-1)
+
+acc = accuracy_score(y_test, preds)
+mcm = multilabel_confusion_matrix(y_test, preds)
+
+print("\nCONVOLUTIONAL NEURAL NETWORK")
+print(f"Accuracy: {acc}")
+
